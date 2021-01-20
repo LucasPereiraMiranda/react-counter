fa8231fc9ca1c5daa050eb328608a1cbeb730059
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-20 16:56:16 -0300

    Criar estilo global

---
+++ src/global/styles.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    font-family:sans-serif;
+  }
+
+  body{
+    font-size: 3rem;
+    padding:0;
+    border:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+`;
