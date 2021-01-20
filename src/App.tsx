f92398ca2462d4e39372136b6b1ecd54c7f1e744
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-20 16:58:27 -0300

    Adicionar App.tsx e index.tsx e react-app-env.ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { GlobalStyle } from './global/styles';
+import Main from './pages/Main';
+
+const App = () => (
+  <>
+    <GlobalStyle />
+    <Main />
+  </>
+);
+
+export default App;
