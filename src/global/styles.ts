f91134fff23ce19892e1fe10b0221d1b54f1903b
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-02-04 15:43:07 -0300

    Definir margin como 0 e outros detalhes

--- src/global/styles.ts
+++ src/global/styles.ts
@@ -3,13 +3,17 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   *{
     font-family:sans-serif;
-  }
-
-  body{
-    font-size: 3rem;
     padding:0;
     border:0;
     padding:0;
+  }
+
+  body{
+    background-color:#fffaaa;
     box-sizing:border-box;
+    min-height:100vh;
+    max-width:100%;
+    font-size: 3rem;
+    margin:0;
   }
 `;
